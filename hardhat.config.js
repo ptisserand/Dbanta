0c2b1b6ce0f424a8f6cf51ffc77bd9ce4fd373c6
Patrice Tisserand <p.tisserand@gmail.com>
2022-07-14 17:00:52 +0200

    hardhat.config: add mumbai and polygon networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,31 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.8",
+  solidity: {
+    version: "0.8.8",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
+  },
+  networks: {
+    mumbai: {
+      chainId: 80001,
+      url: process.env.MUMBAI_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    polygon: {
+      chainId: 137,
+      url: process.env.POLYGON_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  }, 
   paths: {
     root: "./hardhat"
   }
