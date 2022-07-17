1afda61bbbc83b20c4955a2b6293eaad977a25e0
Patrice Tisserand <p.tisserand@gmail.com>
2022-07-17 20:28:25 +0200

    feat(hardhat): update deploy script

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -4,9 +4,16 @@ async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contract with the account:", deployer.address);
     console.log("Account balance:", (await deployer.getBalance()).toString());
+    const nftFactory = await ethers.getContractFactory("DbantaNFT");
+    const nft = await nftFactory.deploy();
     const DbantaFactory = await ethers.getContractFactory("Dbanta");
-    const dbanta = await DbantaFactory.deploy();
+    const dbanta = await DbantaFactory.deploy(nft.address);
+    await nft.deployed();
+    const minter_role = await nft.MINTER_ROLE();
+    console.log("Set minter role to Dbanta contract");
+    await nft.grantRole(minter_role, dbanta.address);
     await dbanta.deployed();
+    console.log("NFT contract deployed to:", nft.address);
     console.log("Deployed to:", dbanta.address);
 }
 
