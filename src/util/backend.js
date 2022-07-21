8e73a0e35d9ab49dfdfc6b7292bfc7d6e297b7f5
Patrice Tisserand <p.tisserand@gmail.com>
2022-07-21 13:23:48 +0200

    feat(tron): WIP support for TRON blockchain

--- src/util/backend.js
+++ src/util/backend.js
@@ -1,24 +1,87 @@
-
-function sleep(ms) {
+const sleep = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export const getUserInfo = async (contract, address) => {
-    let user = await contract.getUser(address);
-    return {
-        name: user.name,
-        username: user.username,
-        image: user.imghash,
-        bio: user.bio
-    }
+const format_bant = (bant) => {
+    return bant;
 }
 
-export const fetchPosts = async (contract, address) => {
-    console.info("Fetching posts...");
-    return [];
+export class TronContract {
+    constructor(contract) {
+        this.contract = contract;
+    };
+    async getUserInfo(address) {
+        console.log("getuserInfo:", address);
+        let user = await this.contract
+            .getUser(address.hex)
+            .call();
+        console.log(user);
+        return {
+            name: user.name,
+            username: user.username,
+            image: user.imghash,
+            bio: user.bio
+        }
+    };
+
+    async fetchPosts(address) {
+        console.info("Fetching posts...");
+        let bants = await this.contract
+            .getUserBants(address.hex)
+            .call();
+        return bants.map(format_bant);
+    };
+
+    async fetchPost(id) {
+        let bant = await this.contract
+            .getBant(id)
+            .call();
+        console.log(bant);
+        return format_bant(bant);
+    };
+
+    async createPost(data) {
+        try {
+            let tx = await this.contract
+                .createBant(data.hashtag, data.content, data.imgHash)
+                .send();
+            console.log(tx);
+            return tx;
+        } catch (err) {
+            console.error(err);
+            return "";
+        }
+    };
 }
 
-export const createPost = async (contract, data) => {
-    let tx = await contract.createBant(data.hashtag, data.content, data.imgHash);
-    return tx;
+export class PolygonContract {
+    constructor(contract) {
+        this.contract = contract;
+    };
+    async getUserInfo(address) {
+        let user = await this.contract.getUser(address);
+        return {
+            name: user.name,
+            username: user.username,
+            image: user.imghash,
+            bio: user.bio
+        }
+    };
+
+    async fetchPosts(address) {
+        console.info("Fetching posts...");
+        let bants = await this.contract.getUserBants(address);
+        return bants.map(format_bant);
+    };
+
+    async fetchPost(id) {
+        let bant = await this.contract.getBant(id);
+        console.log(bant);
+        return format_bant(bant);
+    };
+
+    async createPost(data) {
+        let tx = await this.contract.createBant(data.hashtag, data.content, data.imgHash);
+        return tx;
+    };
 }
