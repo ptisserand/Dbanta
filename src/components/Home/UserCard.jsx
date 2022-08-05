bd33af8e7576a39c21b8e44638120d3776952e3c
Patrice Tisserand <p.tisserand@gmail.com>
2022-08-05 12:49:18 +0200

    feat: add support to follow user

--- src/components/Home/UserCard.jsx
+++ src/components/Home/UserCard.jsx
@@ -4,18 +4,53 @@ import {
   Heading,
   HStack,
   IconButton,
+  Spinner,
   Text,
+  useToast
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { BiUserPlus } from 'react-icons/bi';
+import { useACtx } from '../../context/AuthContext';
 
-function UserCard({ name, username }) {
+function UserCard({ name, username, id }) {
+  const toast = useToast();
+  const { isAuth, contract } = useACtx();
+  const [loading, setLoading] = useState(false);
+
+  const followUser = async () => {
+    console.info("Following ", name, id);
+    if (isAuth !== false && contract !== undefined) {
+      setLoading(true);
+      try {
+      const user = await contract.getUserAddress(id.toNumber());
+      const tx = await contract.followUser(user);
+      toast({
+        status: 'success',
+        title: 'User followed',
+        description: `Transaction id: ${tx}`,
+        duration: 2000,
+        isClosable: true
+      });
+      setLoading(false);
+      } catch(error) {
+        toast({
+          status: 'error',
+          title: 'Fail to follow user',
+          description: `${error.message}`,
+          duration: 3000,
+          isClosable: true,
+        });
+        setLoading(false);
+      }
+    }
+  }
+
   return (
     <HStack justifyContent={'space-between'}>
       <HStack spacing={'2'}>
         <Avatar
           size="sm"
-          name="John Doe"
+          name={name}
           src="https://source.unsplash.com/random/50x50/?portrait"
         />
         <Box>
@@ -32,7 +67,9 @@ function UserCard({ name, username }) {
         variant="flushed"
         icon={<BiUserPlus />}
         fontSize="3xl"
+        onClick={followUser}
       />
+    {loading && <Spinner></Spinner>}
     </HStack>
   );
 }
